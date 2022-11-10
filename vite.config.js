593ffe7064f253772f3316470b3bf5c35466dbf7
overa90 <evan_tottu@live.com.mx>
2022-11-10 11:50:45 -0600

    FIX BUG2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://overa90.github.io/',
+  base:'https://overa90.github.io/gh-react-vite',
   plugins: [react()]
 })
